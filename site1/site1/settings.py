a1cc3597d4709bcb7355cd055c15486bda59682c
Marcukko <marcushuynh250@gmail.com>
2026-06-09 13:25:51 +1000

    fix SQL error

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,14 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql' ,
+        'NAME': 'site1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
+
+
     }
 }
 
